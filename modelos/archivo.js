fbb03ff7570d4a4a88043cf92456bbd91dcf5b4c
Leonel Bongiovanni <labongiovanni@live.com.ar>
2019-11-28 01:53:42 -0300

    Creada relacion entre archivo y user en el modelo

--- modelos/archivo.js
+++ modelos/archivo.js
@@ -2,13 +2,15 @@
 
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
+const users = require('./user');
 
 const ArchivoSchema = schema({
     name: String,
     size: Number,
     type: String,
     location: String,
-    add_Date: String
+    add_Date: String,
+    user: { type: schema.ObjectId, ref: 'User'}
 });
 
 module.exports = mongoose.model('Archivo', ArchivoSchema);
